9f6dcd37c0286649227b6cb32887e30b2a4efc2e
mckrava <mckrava2@gmail.com>
2025-04-28 13:17:23 -0400

    fix: fixed collecting Stablepool liquidity events

--- indexers/liquidity-pools/src/handlers/pools/stableswap/index.ts
+++ indexers/liquidity-pools/src/handlers/pools/stableswap/index.ts
@@ -19,13 +19,15 @@ export async function handleStablepools(
     await stableswapCreated(ctx, eventData);
   }
 
-  await ctx.store.save(
-    [...ctx.batchState.state.stableswapAllBatchPools.values()].filter((pool) =>
-      ctx.batchState.state.stableswapIdsToSave.has(pool.id)
-    )
-  );
-  ctx.batchState.state.stableswapIdsToSave = new Set();
-
+  // await ctx.store.save(
+  //   [...ctx.batchState.state.stableswapAllBatchPools.values()].filter((pool) =>
+  //     ctx.batchState.state.stableswapIdsToSave.has(pool.id)
+  //   )
+  // );
+  // ctx.batchState.state.stableswapIdsToSave = new Set();
+  await ctx.store.save([
+    ...ctx.batchState.state.stableswapAllBatchPools.values(),
+  ]);
   await ctx.store.save([
     ...ctx.batchState.state.stableswapAssetsAllBatch.values(),
   ]);
