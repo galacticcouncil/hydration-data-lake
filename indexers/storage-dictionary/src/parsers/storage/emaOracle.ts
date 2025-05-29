adf39fdad79ba2fbb293ea93663f42326d373c47
mckrava <mckrava2@gmail.com>
2025-05-28 23:05:54 -0400

    fix: fixed emaOracle storage fetch

--- indexers/storage-dictionary/src/parsers/storage/emaOracle.ts
+++ indexers/storage-dictionary/src/parsers/storage/emaOracle.ts
@@ -9,93 +9,108 @@ async function getOracles({
 }: GetEmaOraclesInput): Promise<EmaOracleEntryData[]> {
   if (block.specVersion < 138) return [];
 
-  if (storage.emaOracle.oracles.v138.is(block)) {
+  if (
+    storage.emaOracle.oracles.v138.is(block) ||
+    (block.specVersion >= 138 && block.specVersion < 170)
+  ) {
     const pairsPaged = [];
 
-    for await (const page of storage.emaOracle.oracles.v138.getPairsPaged(
-      500,
-      block
-    )) {
-      pairsPaged.push(
-        ...page
-          .filter((p) => !!p && !!p[1])
-          .map(
-            ([
-              [source, assetIds, period],
-              entryData,
-            ]): EmaOracleEntryData | null => {
-              if (!entryData) return null;
-              const entry = entryData[0];
-              return {
-                source: hexToString(source),
-                assetIds,
-                period: period.__kind as EmaOraclePeriod,
-                price: {
-                  n: entry.price.n,
-                  d: entry.price.d,
-                },
-                volume: {
-                  aIn: entry.volume.aIn,
-                  aOut: entry.volume.aOut,
-                  bIn: entry.volume.bIn,
-                  bOut: entry.volume.bOut,
-                },
-                liquidity: {
-                  a: entry.liquidity.a,
-                  b: entry.liquidity.b,
-                },
-                updatedAt: entry.timestamp,
-              };
-            }
-          )
-          .filter((resp) => !!resp)
-      );
+    try {
+      for await (const page of storage.emaOracle.oracles.v138.getPairsPaged(
+        500,
+        block
+      )) {
+        pairsPaged.push(
+          ...page
+            .filter((p) => !!p && !!p[1])
+            .map(
+              ([
+                [source, assetIds, period],
+                entryData,
+              ]): EmaOracleEntryData | null => {
+                if (!entryData) return null;
+                const entry = entryData[0];
+                return {
+                  source: hexToString(source),
+                  assetIds,
+                  period: period.__kind as EmaOraclePeriod,
+                  price: {
+                    n: entry.price.n,
+                    d: entry.price.d,
+                  },
+                  volume: {
+                    aIn: entry.volume.aIn,
+                    aOut: entry.volume.aOut,
+                    bIn: entry.volume.bIn,
+                    bOut: entry.volume.bOut,
+                  },
+                  liquidity: {
+                    a: entry.liquidity.a,
+                    b: entry.liquidity.b,
+                  },
+                  updatedAt: entry.timestamp,
+                };
+              }
+            )
+            .filter((resp) => !!resp)
+        );
+      }
+    } catch (e) {
+      console.log('storage.emaOracle.oracles.v138 has failed');
+      console.log(e);
     }
+
     return pairsPaged;
   }
 
-  if (storage.emaOracle.oracles.v170.is(block)) {
+  if (storage.emaOracle.oracles.v170.is(block) || block.specVersion >= 170) {
     const pairsPaged = [];
 
-    for await (const page of storage.emaOracle.oracles.v170.getPairsPaged(
-      500,
-      block
-    )) {
-      pairsPaged.push(
-        ...page
-          .filter((p) => !!p && !!p[1])
-          .map(
-            ([
-              [source, assetIds, period],
-              entryData,
-            ]): EmaOracleEntryData | null => {
-              if (!entryData) return null;
-              const entry = entryData[0];
-              return {
-                source: hexToString(source),
-                assetIds: assetIds,
-                period: period.__kind as EmaOraclePeriod,
-                price: {
-                  n: entry.price.n,
-                  d: entry.price.d,
-                },
-                volume: {
-                  aIn: entry.volume.aIn,
-                  aOut: entry.volume.aOut,
-                  bIn: entry.volume.bIn,
-                  bOut: entry.volume.bOut,
-                },
-                liquidity: {
-                  a: entry.liquidity.a,
-                  b: entry.liquidity.b,
-                },
-                updatedAt: entry.updatedAt,
-              };
-            }
-          )
-          .filter((resp) => !!resp)
-      );
+    try {
+      for await (const page of storage.emaOracle.oracles.v170.getPairsPaged(
+        500,
+        block
+      )) {
+        pairsPaged.push(
+          ...page
+            .filter((p) => !!p && !!p[1])
+            .map(
+              ([
+                [source, assetIds, period],
+                entryData,
+              ]): EmaOracleEntryData | null => {
+                if (!entryData) return null;
+                const entry = entryData[0];
+                return {
+                  source: hexToString(source),
+                  assetIds: assetIds,
+                  period: period.__kind as EmaOraclePeriod,
+                  price: {
+                    n: entry.price.n,
+                    d: entry.price.d,
+                  },
+                  volume: {
+                    aIn: entry.volume.aIn,
+                    aOut: entry.volume.aOut,
+                    bIn: entry.volume.bIn,
+                    bOut: entry.volume.bOut,
+                  },
+                  liquidity: {
+                    a: entry.liquidity.a,
+                    b: entry.liquidity.b,
+                  },
+                  updatedAt: entry.updatedAt,
+                };
+              }
+            )
+            .filter((resp) => !!resp)
+        );
+      }
+    } catch (e) {
+      console.log('storage.emaOracle.oracles.v170 has failed');
+      console.log(e);
     }
+
     return pairsPaged;
   }
 
