be46d824bec1b017ecdf00a893465359f613cccc
mckrava <mckrava2@gmail.com>
2025-05-29 06:17:31 -0400

    fix: fixed getAssetFeesAll fetching

--- indexers/storage-dictionary/src/parsers/storage/dynamicFees.ts
+++ indexers/storage-dictionary/src/parsers/storage/dynamicFees.ts
@@ -11,28 +11,33 @@ async function getAssetFeesAll({
 }: GetAssetsDynamicFeesAllInput): Promise<Array<AssetDynamicFeeData>> {
   if (block.specVersion < 170) return [];
 
-  if (storage.dynamicFees.assetFee.v170.is(block)) {
+  if (storage.dynamicFees.assetFee.v170.is(block) || block.specVersion >= 170) {
     const pairsPaged = [];
 
-    for await (const page of storage.dynamicFees.assetFee.v170.getPairsPaged(
-      500,
-      block
-    )) {
-      pairsPaged.push(
-        ...page
-          .filter((p) => !!p && !!p[1])
-          .map(([assetId, fees]): AssetDynamicFeeData | null => {
-            if (!fees) return null;
+    try {
+      for await (const page of storage.dynamicFees.assetFee.v170.getPairsPaged(
+        500,
+        block
+      )) {
+        pairsPaged.push(
+          ...page
+            .filter((p) => !!p && !!p[1])
+            .map(([assetId, fees]): AssetDynamicFeeData | null => {
+              if (!fees) return null;
 
-            return {
-              assetId,
-              assetFee: fees.assetFee,
-              protocolFee: fees.protocolFee,
-              timestamp: fees.timestamp,
-            };
-          })
-          .filter((resp) => !!resp)
-      );
+              return {
+                assetId,
+                assetFee: fees.assetFee,
+                protocolFee: fees.protocolFee,
+                timestamp: fees.timestamp,
+              };
+            })
+            .filter((resp) => !!resp)
+        );
+      }
+    } catch (e) {
+      console.log('storage.dynamicFees.assetFee.v170 has failed');
+      console.log(e);
     }
     return pairsPaged;
   }
