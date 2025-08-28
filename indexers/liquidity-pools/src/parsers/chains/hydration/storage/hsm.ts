995e4960ec980eb99a41722a9a3711dbcdb92131
mckrava <mckrava2@gmail.com>
2025-08-28 13:03:07 -0400

    fix: fixed hsm storage reading

--- indexers/liquidity-pools/src/parsers/chains/hydration/storage/hsm.ts
+++ indexers/liquidity-pools/src/parsers/chains/hydration/storage/hsm.ts
@@ -6,20 +6,25 @@ import {
 } from '../../../types/storage';
 import { UnknownVersionError } from '../../../../utils/errors';
 import { storage } from '../typegenTypes/';
+import { tryExecOrReturnFallback } from '../../../../utils/helpers';
 
 async function getCollateral({
   collateralId,
   block,
 }: GetHsmCollateralInput): Promise<HsmCollateralData | null> {
-  if (storage.hsm.collaterals.v323.is(block)) {
-    const resp = await storage.hsm.collaterals.v323.get(block, +collateralId);
+  if (block.specVersion < 323) return null;
 
-    return resp
-      ? {
-          collateralAssetId: +collateralId,
-          ...resp,
-        }
-      : null;
+  if (storage.hsm.collaterals.v323.is(block)) {
+    return tryExecOrReturnFallback(async () => {
+      const resp = await storage.hsm.collaterals.v323.get(block, +collateralId);
+
+      return resp
+        ? {
+            collateralAssetId: +collateralId,
+            ...resp,
+          }
+        : null;
+    }, null);
   }
 
   throw new UnknownVersionError('storage.hsm.collaterals');
@@ -28,20 +33,24 @@ async function getCollateral({
 async function getAllCollaterals({
   block,
 }: GetDataAtBlockInput): Promise<HsmCollateralData[] | null> {
+  if (block.specVersion < 323) return null;
+
   if (storage.hsm.collaterals.v323.is(block)) {
-    const resp = await storage.hsm.collaterals.v323.getPairs(block);
-    const result: HsmCollateralData[] = [];
+    return tryExecOrReturnFallback(async () => {
+      const resp = await storage.hsm.collaterals.v323.getPairs(block);
+      const result: HsmCollateralData[] = [];
 
-    for (const [collateralAssetId, collateralData] of resp) {
-      if (!collateralData) continue;
+      for (const [collateralAssetId, collateralData] of resp) {
+        if (!collateralData) continue;
 
-      result.push({
-        collateralAssetId,
-        ...collateralData,
-      });
-    }
+        result.push({
+          collateralAssetId,
+          ...collateralData,
+        });
+      }
 
-    return result.length ? result : null;
+      return result.length ? result : null;
+    }, null);
   }
 
   throw new UnknownVersionError('storage.hsm.collaterals');
