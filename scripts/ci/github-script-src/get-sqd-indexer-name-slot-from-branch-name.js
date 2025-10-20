2dae695ea9787584e0c6504dfc537778f3308296
mckrava <mckrava2@gmail.com>
2025-10-20 14:13:06 -0400

    fix: debug 5

--- scripts/ci/github-script-src/get-sqd-indexer-name-slot-from-branch-name.js
+++ scripts/ci/github-script-src/get-sqd-indexer-name-slot-from-branch-name.js
@@ -20,13 +20,12 @@ module.exports = async ({ github, context }) => {
     throw new Error("Missing branch name");
   }
 
-  const [folderName, indexerName, indexerSlot] = branchName.split("/");
+  const [folderName = "", indexerName = "", indexerSlot = ""] =
+    branchName.split("/");
 
   console.log("folderName", folderName);
   console.log("indexerName", indexerName);
   console.log("indexerSlot", indexerSlot);
 
-  // if (!folderName || folderName !== VERSIONS_ROOT_FOLDER_NAME)
-  // return JSON.stringify({ indexerName, indexerSlot });
   return { indexerName, indexerSlot };
 };
