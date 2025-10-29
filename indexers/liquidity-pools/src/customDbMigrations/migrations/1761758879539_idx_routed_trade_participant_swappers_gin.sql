6ae995c5ea9ab3ac80a6822182c98f5e01394fa2
mckrava <mckrava2@gmail.com>
2025-10-29 14:48:57 -0400

    fix: fixed indexes creation db migrations

--- indexers/liquidity-pools/src/customDbMigrations/migrations/1761758879539_idx_routed_trade_participant_swappers_gin.sql
+++ indexers/liquidity-pools/src/customDbMigrations/migrations/1761758879539_idx_routed_trade_participant_swappers_gin.sql
@@ -1,2 +1,11 @@
-CREATE INDEX IF NOT EXISTS idx_routed_trade_participant_swappers_gin
-    ON "routed_trade" USING GIN ("participant_swappers");
+DO $$
+BEGIN
+    IF NOT EXISTS (
+        SELECT 1 FROM pg_indexes
+        WHERE indexname = 'idx_routed_trade_participant_swappers_gin'
+    ) THEN
+        CREATE INDEX IF NOT EXISTS idx_routed_trade_participant_swappers_gin
+        ON "routed_trade" USING GIN ("participant_swappers");
+    END IF;
+END
+$$;
