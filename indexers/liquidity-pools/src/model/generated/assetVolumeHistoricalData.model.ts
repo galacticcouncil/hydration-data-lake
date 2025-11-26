92fb90ffcbf144c98251375f540c010840fee0f6
mckrava <mckrava2@gmail.com>
2025-11-26 11:49:26 -0300

    fix: fixed db migration

--- indexers/liquidity-pools/src/model/generated/assetVolumeHistoricalData.model.ts
+++ indexers/liquidity-pools/src/model/generated/assetVolumeHistoricalData.model.ts
@@ -31,17 +31,17 @@ export class AssetVolumeHistoricalData {
   @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
   totalVolumeOut!: bigint
 
-  @Column_("text", {nullable: false})
-  volumeInNorm!: string
+  @Column_("text", {nullable: true})
+  volumeInNorm!: string | undefined | null
 
-  @Column_("text", {nullable: false})
-  volumeOutNorm!: string
+  @Column_("text", {nullable: true})
+  volumeOutNorm!: string | undefined | null
 
-  @Column_("text", {nullable: false})
-  totalVolumeInNorm!: string
+  @Column_("text", {nullable: true})
+  totalVolumeInNorm!: string | undefined | null
 
-  @Column_("text", {nullable: false})
-  totalVolumeOutNorm!: string
+  @Column_("text", {nullable: true})
+  totalVolumeOutNorm!: string | undefined | null
 
   @Index_()
   @Column_("int4", {nullable: false})
