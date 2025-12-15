c512078fd4eb7c2245efa422865530a2813437b8
mckrava <mckrava2@gmail.com>
2025-12-15 13:00:10 -0500

    fix: fixed collection fee on Stableswap_LiquidityRemoved

--- indexers/liquidity-pools/src/handlers/pools/pools/stableswap/liquidity.ts
+++ indexers/liquidity-pools/src/handlers/pools/pools/stableswap/liquidity.ts
@@ -10,7 +10,10 @@ import {
   StableswapLiquidityEvent,
 } from '../../../../model';
 import { getOrCreateStableswap } from './stablepool';
-import { EventName } from '../../../../parsers/types/events';
+import {
+  EventName,
+  StableswapLiquidityRemovedEventParams,
+} from '../../../../parsers/types/events';
 import { getOrCreateAsset } from '../../../assets/asset';
 import { handleStablepoolVolumeUpdates } from '../../volumes/stablepoolVolume';
 import {
@@ -46,12 +49,8 @@ export async function handleStablepoolLiquidityEvents(
     );
   }
 
-  await ctx.store.save([
-    ...ctx.batchState.state.stableswapPools.values(),
-  ]);
-  await ctx.store.save([
-    ...ctx.batchState.state.stableswapAssets.values(),
-  ]);
+  await ctx.store.save([...ctx.batchState.state.stableswapPools.values()]);
+  await ctx.store.save([...ctx.batchState.state.stableswapAssets.values()]);
 
   await ctx.store.save([
     ...ctx.batchState.state.stablepoolBatchLiquidityActions.values(),
@@ -83,7 +82,12 @@ export async function stablepoolLiquidityAddedRemoved(
 
   if (!pool) return;
 
-  const fee = BigInt(0);
+  const fee =
+    eventCallData.eventData.metadata.name ===
+    EventName.Stableswap_LiquidityRemoved
+      ? (eventParams as StableswapLiquidityRemovedEventParams).fee
+      : BigInt(0);
+
   let assetAmounts = [];
 
   const actionType =
