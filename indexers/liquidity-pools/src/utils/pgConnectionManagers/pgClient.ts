b37949d073b1565a232242ed6ea5dd97fcad498b
mckrava <mckrava2@gmail.com>
2026-02-02 09:34:29 -0500

    fix: changed pgClient init

--- indexers/liquidity-pools/src/utils/pgConnectionManagers/pgClient.ts
+++ indexers/liquidity-pools/src/utils/pgConnectionManagers/pgClient.ts
@@ -1,8 +1,6 @@
 import { AppConfig } from '../../appConfig';
 import { Client, QueryResult, QueryResultRow } from 'pg';
 
-const appConfig = AppConfig.getInstance();
-
 export class CommonPgClient {
   private static instance: CommonPgClient;
 
@@ -16,6 +14,8 @@ export class CommonPgClient {
   }
 
   constructor() {
+    const appConfig = AppConfig.getInstance();
+
     this.pgClient = new Client({
       host: appConfig.DB_HOST,
       port: appConfig.DB_PORT,
@@ -31,10 +31,25 @@ export class CommonPgClient {
     maxDelayMs = 10000
   ): Promise<void> {
     let attempt = 0;
+    const appConfig = AppConfig.getInstance();
 
     while (true) {
+      console.log('CommonPgClient :: p', appConfig.DB_PASS);
       try {
-        await this.pgClient.connect();
+        // Create a new client for each attempt
+        const client = new Client({
+          host: appConfig.DB_HOST,
+          port: appConfig.DB_PORT,
+          database: appConfig.DB_NAME,
+          user: appConfig.DB_USER,
+          password: appConfig.DB_PASS,
+        });
+
+        await client.connect();
+
+        // Only assign to this.pgClient after successful connection
+        this.pgClient = client;
+
         console.log(
           '[PostgreSQL :: CommonPgClient] connection established successfully'
         );
