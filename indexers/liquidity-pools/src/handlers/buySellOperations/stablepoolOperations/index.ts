03d4fa34cda528ee1175c4a9e2855aea5fe0c0e7
mckrava <mckrava2@gmail.com>
2026-03-15 16:26:52 -0400

    feat: fixed stableswap fee collecting

--- indexers/liquidity-pools/src/handlers/buySellOperations/stablepoolOperations/index.ts
+++ indexers/liquidity-pools/src/handlers/buySellOperations/stablepoolOperations/index.ts
@@ -144,7 +144,10 @@ export async function stablepoolBuySellExecuted(
       fees: [
         {
           amount: eventParams.fee,
-          assetId: eventParams.assetOut,
+          assetId:
+            eventMetadata.name === EventName.Stableswap_BuyExecuted
+              ? eventParams.assetIn
+              : eventParams.assetOut,
           destinationType: SwapFeeDestinationType.Account,
           recipientId: pool.account.id,
         },
